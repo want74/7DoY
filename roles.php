fe5e22298d6b8f2c6a130b1628fa611956cc3cb4
K4N <andrushkakychkin@gmail.com>
2021-05-05 06:10:04 +0900

    Доделал модальное окно

--- roles.php
+++ roles.php
@@ -25,7 +25,7 @@
             <div class="col-7 mx-auto bg-dark">
                 <div class="row" style="height:270px;">
                     <div class="col bg-warning d-flex">
-                        <p class="mx-auto my-auto">Разработчик</p>
+                        <a class="mx-auto my-auto" data-toggle="modal" data-target="#Description">Разработчик</a>
                     </div>
                     <div class="col bg-primary d-flex">
                         <p class="mx-auto my-auto">Менеджер</p>
@@ -37,6 +37,28 @@
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="Description" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Мой опыт:</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="asdasd">
+                        <div class="form-group">
+                            <textarea class="form-control" id="message-text"></textarea>
+                        </div>
+                        <button class="btn btn-success">Отправить</button>
+
+                    </form>
+                </div>
+        </div>
+    </div>
+
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
